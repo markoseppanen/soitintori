d229170c691fada29cea86c531f35905b1d3d61a
RiQhy <riku.nokelainen@metropolia.fi>
2023-09-25 10:03:00 +0300

    added utilities

---
+++ utils/app-config.js
@@ -0,0 +1,5 @@
+const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
+const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const appId = 'SoitinTori';
+
+export {apiUrl, mediaUrl, appId};
