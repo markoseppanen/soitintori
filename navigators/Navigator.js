0baf1b9e8d9620c9499717fa48fe0b19a2fe0c46
Marko Seppänen <marko@marko.xyz>
2023-09-27 12:20:33 +0300

    removed unnecessary fragment

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,46 +1,33 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { Instruments } from '../views/Instruments';
-import { SingleInstrument } from '../views/SingleInstrument';
-import { Categories } from '../views/Categories';
-import { AddListing } from '../views/AddListing';
-import { Profile } from '../views/Profile';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {Instruments} from '../views/Instruments';
+import {SingleInstrument} from '../views/SingleInstrument';
+import {Categories} from '../views/Categories';
+import {AddListing} from '../views/AddListing';
+import {Profile} from '../views/Profile';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const TabScreen = () => (
   <Tab.Navigator>
-    <Tab.Screen
-      name="Categories"
-      component={Categories}
-    />
-    <Tab.Screen
-      name="Add Listing"
-      component={AddListing}
-    />
-    <Tab.Screen
-      name="Profile"
-      component={Profile}
-    />
+    <Tab.Screen name="Categories" component={Categories} />
+    <Tab.Screen name="Add Listing" component={AddListing} />
+    <Tab.Screen name="Profile" component={Profile} />
   </Tab.Navigator>
 );
 
 const StackScreen = () => {
   return (
     <Stack.Navigator>
-      <>
-        <Stack.Screen
-          name="Tabs"
-          component={TabScreen}
-          options={{ headerShown: false }}
-        />
-      
-        <Stack.Screen name="Instruments" component={Instruments} />
-        <Stack.Screen name="SingleInstrument" component={SingleInstrument} />
-      </>
-      
+      <Stack.Screen
+        name="Tabs"
+        component={TabScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Instruments" component={Instruments} />
+      <Stack.Screen name="SingleInstrument" component={SingleInstrument} />
     </Stack.Navigator>
   );
 };
